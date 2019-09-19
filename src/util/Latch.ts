52c493e7e4c4e78c3a4962dd938a6834c93dfab5
burton <burton@getpolarized.io>
2019-09-19 10:14:42 -0700

    migrate Latch to polar-shared

--- src/util/Latch.ts
+++ src/util/Latch.ts
@@ -1,15 +1,17 @@
 /**
- * A latch that can be resolved. Like a ResolveablePromise by
+ * A latch that can be resolved. Like a ResolvablePromise by
  */
 export class Latch<T> {
 
     private promise: Promise<T>;
 
     // noinspection TsLint
-    private _resolve: (value?: T) => void = () => { };
+    private _resolve: (value?: T) => void = () => {
+    };
 
     // noinspection TsLint
-    private _reject: (reason?: any) => void = () => { };
+    private _reject: (reason?: any) => void = () => {
+    };
 
     constructor() {
 
@@ -27,12 +29,12 @@ export class Latch<T> {
     // noinspection TsLint
     public resolve = (value: T) => {
         this._resolve(value);
-    };
+    }
 
     // noinspection TsLint
     public reject = (reason: any) => {
         this._reject(reason);
-    };
+    }
 
     // noinspection TsLint
     public get = (): Promise<T> => {
