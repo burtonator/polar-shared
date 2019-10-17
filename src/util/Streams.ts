453ba0e0be94c7e6d71f73238c4751a534c17ac1
burton <burton@getpolarized.io>
2019-10-16 19:48:26 -0700

    ...

--- src/util/Streams.ts
+++ src/util/Streams.ts
@@ -1,9 +1,19 @@
 import * as readline from 'readline';
-import {ProgressListener, ProgressTracker, ProgressTrackerInit} from "./ProgressTracker";
+import {Progress, ProgressListener, ProgressTracker, ProgressTrackerInit} from "./ProgressTracker";
+import {PassThrough, Duplex} from "stream";
 
 export class Streams {
 
     public static isValidStream(stream: NodeJS.ReadableStream): boolean {
+
+        if (! stream) {
+            return false;
+        }
+
+        if (! stream.read()) {
+
+        }
+
         return !(!stream || !stream.readable);
     }
 
@@ -11,8 +21,12 @@ export class Streams {
 
         return new Promise((resolve, reject) => {
 
-            if (! this.isValidStream(stream)) {
-                reject(new Error('Either Stream Ended or not valid Stream'));
+            if (! stream) {
+                reject(new Error("No stream given"));
+            }
+
+            if (! stream.readable) {
+                reject(new Error("Stream not readable"));
             }
 
             const array: Uint8Array[] = [];
@@ -80,20 +94,53 @@ export class Streams {
                                    init: ProgressTrackerInit,
                                    listener: ProgressListener) {
 
-        let bytesRead = 0;
+        // https://github.com/freeall/progress-stream/blob/master/index.js
 
         const progressTracker = new ProgressTracker(init);
 
-        listener(progressTracker.peek());
+        const resultStream = new PassThrough();
+        const listenerStream = new PassThrough();
 
-        stream.on('data', (chunk: Uint8Array) => {
-            bytesRead += chunk.length;
+        resultStream.once('resume', () => {
+
+            // this always needs to be called but we might want to only start calling it on resume this is
+            // when the user of the stream has signified that they're actually using it which might be
+            // at any random time.
+            listener(progressTracker.peek());
+
+            // once the FIRST resume is called, start pipe data through. First to our listener stream and then
+            // to our result stream.
+            listenerStream.pipe(resultStream);
+            stream.pipe(listenerStream);
+
         });
 
-        stream.on('end', () => {
+        let progress: Progress | undefined;
+
+        // https://www.freecodecamp.org/news/node-js-streams-everything-you-need-to-know-c9141306be93/
+        //
+        // In the flowing mode, data can actually be lost if no consumers are available to handle it. This is why, when
+        // we have a readable stream in flowing mode, we need a data event handler. In fact, just adding a data event
+        // handler switches a paused stream into flowing mode and removing the data event handler switches the stream
+        // back to paused mode.
+
+        listenerStream.on('data', (chunk: Uint8Array) => {
+            // we have data now call the progress trackerlistener
+            progress = progressTracker.incr(chunk.length);
+            listener(progress);
+        });
+
+        listenerStream.on('end', () => {
             // issue one final event when we're done.
-            listener(progressTracker.terminate());
+
+            if (! progress || progress.completed < progress.total) {
+                // make sure we always get at least one event
+                listener(progressTracker.terminate());
+            }
+
         });
+
+        return resultStream;
 
     }
 
--- src/util/StreamsTest.ts
+++ src/util/StreamsTest.ts
@@ -2,20 +2,41 @@ import {assert} from 'chai';
 import {Files} from "./Files";
 import {FilePaths} from "./FilePaths";
 import {Streams} from "./Streams";
+import {Strings} from "./Strings";
+import {assertJSON} from "polar-test/src/test/Assertions";
 
 describe('StreamsTest', function() {
 
     it("Basic", async function() {
 
         let path = FilePaths.createTempName('stream-progress.txt');
-        await Files.writeFileAsync(path, 'xxxxxxxxxxxxxxxxxxxxxxxx');
+
+        const data = Strings.generate(65536 * 3);
+
+        await Files.writeFileAsync(path, data);
 
         const stat = await Files.statAsync(path);
 
         const stream = await Files.createReadStream(path);
 
         let init = {id: 'test', total: stat.size};
-        Streams.toProgressStream(stream, init, (progress) => console.log(progress));;
+
+        const callbacks: number[] = [];
+
+        const progressStream = await Streams.toProgressStream(stream, init, (progress) => {
+            callbacks.push(progress.completed);
+        });
+
+        assertJSON(callbacks, []);
+
+        await Streams.toBuffer(progressStream);
+
+        assertJSON(callbacks, [
+            0,
+            65536,
+            131072,
+            196608
+        ]);
 
     });
 
