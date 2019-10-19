7f9c1e44fff891a362c319fbb58dcb64317a2e12
burton <burton@getpolarized.io>
2019-10-19 15:34:49 -0700

    ...ability to capture the PHZ directly...

--- src/util/Base64.ts
+++ src/util/Base64.ts
@@ -1,3 +1,8 @@
+/**
+ * A Base64 encoded string.
+ */
+export type Base64Str = string;
+
 export class Base64 {
 
     // https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding
